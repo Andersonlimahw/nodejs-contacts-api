4e2a248dc7f45f1108467502f9e862567e132618
Anderson Lima <andersonlimahw@gmail.com>
2023-10-28 06:54:52 -0300

    🆕 [feature] creating project and adding nodemon

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send({ message: '🍋 Hello World!' });
+});
+
+app.listen(3000, () => console.log('🍋 Server is running!'))
