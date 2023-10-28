a1625aa52648538676465fa53e4247e348cd866a
Anderson Lima <andersonlimahw@gmail.com>
2023-10-28 07:14:01 -0300

    🆕 [feature] config eslint and editor config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 
 const app = express();
-
 app.get('/', (request, response) => {
   response.send({ message: '🍋 Hello World!' });
 });
-
-app.listen(3000, () => console.log('🍋 Server is running!'))
+app.listen(3000, () => console.log('🍋 Server is running!'));
